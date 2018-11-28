9604ea7d566db3ed87ff7c426366010506a7030c
Neil Bennett <neil@clypd.com>
2018-11-28 12:14:54 -0500

    MAIN-31443: unused params and clarifying comments

--- dialect_redshift.go
+++ dialect_redshift.go
@@ -4,16 +4,19 @@ type RedshiftDialect struct {
 	PostgresDialect
 }
 
-func (d RedshiftDialect) InsertAutoIncrToTarget(exec SqlExecutor, insertSql string, target interface{}, params ...interface{}) error {
+func (d RedshiftDialect) InsertAutoIncrToTarget(exec SqlExecutor, insertSql string, _ interface{}, params ...interface{}) error {
 	rows, err := exec.Query(insertSql, params...)
 	defer rows.Close()
 	return err
 }
 
 func (d RedshiftDialect) AutoIncrBindValue() string {
+	// returning an empty string here makes gorp skip adding this column to the INSERT SQL
+	// this is needed because Redshift does not allow specifying values for IDENTITY columns
 	return ""
 }
 
 func (d RedshiftDialect) AutoIncrInsertSuffix(col *ColumnMap) string {
+	// Redshift has no support for RETURNING values after an insert
 	return ""
 }
